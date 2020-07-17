594e059e9fb0d3eea28a9f55ada47b1ae54f6a67
ssoraa <kiuy.or@icloud.com>
2020-07-17 17:11:19 +0200

    remove express-session

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express'),
     config = require('./config'),
     {Client} = require('discord.js'),
-    session = require('express-session'),
     app = express(),
     client = new Client();
 
@@ -9,12 +8,6 @@ app.set('view engine', 'ejs');
 client.login(config.botTOKEN)
 
 app.use('/assets', express.static('public'));
-app.use(session({
-    secret: config.secret,
-    resave: false,
-    saveUninitialized: true,
-    cookie: {secure: false}
-}));
 
 app.get('/', (request, response) => {
     response.render('index.ejs');
